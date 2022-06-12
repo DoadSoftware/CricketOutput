ea579d4860424e21647e0a5527e3e67fa03be731
Doad Software Department <98625971+DoadSoftware@users.noreply.github.com>
2022-06-12 14:18:34 +0100

    Add files via upload

--- src/com/cricket/controller/IndexController.java
+++ src/com/cricket/controller/IndexController.java
@@ -52,9 +52,12 @@ public class IndexController
 	public static Socket session_socket;
 	public static Doad this_doad;
 	public static PrintWriter print_writer;
+	 //string static png_extension = '.png';
 	List<BattingCard> last_infobar_batsman;
-	BowlingCard last_bowler;
-	String session_selected_broadcaster, viz_scene_path, which_graphics_onscreen, info_bar_bottom_left, info_bar_bottom_right, info_bar_bottom;
+	BowlingCard last_infobar_bowler;
+	int whichInning,player_id;
+	String session_selected_broadcaster, viz_scene_path, which_graphics_onscreen, info_bar_bottom_left, info_bar_bottom_right, 
+	info_bar_bottom,stats_type,top_left_stats,top_right_stats,type_of_profile;
 	boolean is_Infobar_on_Screen = false;
 	
 	@RequestMapping(value = {"/","/initialise"}, method={RequestMethod.GET,RequestMethod.POST}) 
@@ -104,10 +107,16 @@ public class IndexController
 		info_bar_bottom_right = "";
 		info_bar_bottom = "";
 		which_graphics_onscreen = "";
+		stats_type = "";
+		top_left_stats = "";
+		top_right_stats = "";
+		type_of_profile = "";
+		whichInning = 0;
+		player_id = 0;
 		is_Infobar_on_Screen = false;
 		this_doad = new Doad();
 		last_infobar_batsman = new ArrayList<BattingCard>();
-		last_bowler = new BowlingCard();
+		last_infobar_bowler = new BowlingCard();
 		session_selected_broadcaster = select_broadcaster;
 		session_socket = new Socket(vizIPAddresss, Integer.valueOf(vizPortNumber));
 		print_writer = new PrintWriter(session_socket.getOutputStream(), true);
@@ -157,15 +166,40 @@ public class IndexController
 						new File(CricketUtil.CRICKET_DIRECTORY + CricketUtil.EVENT_DIRECTORY + session_match.getMatchFileName()));
 				
 				session_match.setEvents(session_event_file.getEvents());
-				this_doad.populateScorecard(print_writer, "", 0 , session_match, session_selected_broadcaster, "");
+				//System.out.println("Inning ="+ whichInning);
+				switch(which_graphics_onscreen) {
+				case "SCORECARD":
+					this_doad.populateScorecard(print_writer, viz_scene_path , whichInning , session_match, session_selected_broadcaster);
+					break;
+				case "BOWLINGCARD":
+					this_doad.populateBowlingcard(print_writer, viz_scene_path, whichInning, session_match, session_selected_broadcaster);
+					break;
+				case "PARTNERSHIP":
+					this_doad.populatePartnership(print_writer, viz_scene_path, whichInning, session_match, session_selected_broadcaster);
+					break;
+				case "SUMARRY":
+					this_doad.populateMatchsummary(print_writer, viz_scene_path, whichInning, session_match, session_selected_broadcaster);
+					break;
+				case "HOWOUT":
+					this_doad.populateHowout(print_writer, viz_scene_path, whichInning, stats_type, player_id, session_match, session_selected_broadcaster);
+					break;
+				case "PLAYERSTATS":
+					this_doad.populatePlayerstats(print_writer, viz_scene_path, whichInning, stats_type, player_id, session_match, session_selected_broadcaster);
+					break;
+				/*case "PLAYERPROFILE":
+					this_doad.populatePlayerProfile(print_writer, viz_scene_path, player_id, stats_type, type_of_profile, null, session_match, session_selected_broadcaster);
+					break;*/
+					
+				}
+				
 				if(is_Infobar_on_Screen == true) {
 					
-					last_infobar_batsman = this_doad.populateInfobarTopLeft(true, print_writer, "BATSMAN", session_match, session_selected_broadcaster, last_infobar_batsman);
-					last_bowler = this_doad.populateInfobarTopRight(true, print_writer, "BOWLER", session_match, session_selected_broadcaster,last_bowler);
+					last_infobar_batsman = this_doad.populateInfobarTopLeft(true, print_writer, top_left_stats.toUpperCase(), session_match, session_selected_broadcaster, last_infobar_batsman);
+					last_infobar_bowler = this_doad.populateInfobarTopRight(true, print_writer, top_right_stats.toUpperCase(), session_match, session_selected_broadcaster,last_infobar_bowler);
 					
-					this_doad.populateInfobarTopLeft(true, print_writer, "BATSMAN", session_match, session_selected_broadcaster,last_infobar_batsman);
+					this_doad.populateInfobarTopLeft(true, print_writer, top_left_stats.toUpperCase(), session_match, session_selected_broadcaster,last_infobar_batsman);
 					this_doad.populateInfobarTeamScore(true, print_writer, session_match, session_selected_broadcaster);
-					this_doad.populateInfobarTopRight(true, print_writer, "BOWLER", session_match, session_selected_broadcaster,last_bowler);
+					this_doad.populateInfobarTopRight(true, print_writer, top_right_stats.toUpperCase() , session_match, session_selected_broadcaster,last_infobar_bowler);
 					this_doad.populateInfobarBottomLeft(true, print_writer, info_bar_bottom_left, session_match, session_selected_broadcaster);
 					this_doad.populateInfobarBottomRight(true, print_writer, info_bar_bottom_right, session_match, session_selected_broadcaster);
 					this_doad.populateInfobarBottom(true, print_writer, info_bar_bottom, session_match, session_selected_broadcaster);
@@ -178,70 +212,97 @@ public class IndexController
 				return JSONObject.fromObject(null).toString();
 			}
 		
-		case "POPULATE-SCORECARD": case "POPULATE-BOWLINGCARD": case "POPULATE-PARTNERSHIP": case "POPULATE-MATCHSUMMARY": case "POPULATE-BUG":  case "POPULATE-HOWOUT":
-		case "POPULATE-PLAYERSTATS": case "POPULATE-NAMESUPER": case "POPULATE-PLAYERPROFILE": case "POPULATE-DOUBLETEAMS": case "POPULATE-INFOBAR": case "POPULATE-LEADERBOARD":
-		case "POPULATE-INFOBAR-BOTTOMLEFT": case "POPULATE-INFOBAR-BOTTOMRIGHT": case "POPULATE-INFOBAR-BOTTOM": case "POPULATE-MATCHID": case "POPULATE-PLAYINGXI":
+		case "POPULATE-FF-SCORECARD": case "POPULATE-FF-BOWLINGCARD": case "POPULATE-FF-PARTNERSHIP": case "POPULATE-FF-MATCHSUMMARY": case "POPULATE-L3-BUG":  case "POPULATE-L3-HOWOUT":
+		case "POPULATE-L3-PLAYERSTATS": case "POPULATE-L3-NAMESUPER": case "POPULATE-FF-PLAYERPROFILE": case "POPULATE-FF-DOUBLETEAMS": case "POPULATE-L3-INFOBAR": case "POPULATE-FF-LEADERBOARD":
+		case "POPULATE-INFOBAR-BOTTOMLEFT": case "POPULATE-INFOBAR-BOTTOMRIGHT": case "POPULATE-INFOBAR-BOTTOM": case "POPULATE-FF-MATCHID": case "POPULATE-FF-PLAYINGXI":
 			switch (session_selected_broadcaster.toUpperCase()) {
-			case "DOAD_IN_HOUSE_EVEREST": case "DOAD_IN_HOUSE_VIZ":
-	
-				viz_scene_path = valueToProcess.split(",")[0];
+			
+			case "DOAD_IN_HOUSE_VIZ":
+				break;
+				
+			case "DOAD_IN_HOUSE_EVEREST":
+				
 				switch(whatToProcess.toUpperCase()) {
 				case"POPULATE-INFOBAR-BOTTOMLEFT": case"POPULATE-INFOBAR-BOTTOMRIGHT": case "POPULATE-INFOBAR-BOTTOM":
 					break;
 				default:
+					viz_scene_path = valueToProcess.split(",")[0];
 					new Scene(viz_scene_path).scene_load(new PrintWriter(session_socket.getOutputStream(),true),session_selected_broadcaster,viz_scene_path);
 					break;
 				}
 				switch (whatToProcess.toUpperCase()) {
-				case "POPULATE-SCORECARD":
-					this_doad.populateScorecard(print_writer, valueToProcess.split(",")[0], Integer.valueOf(valueToProcess.split(",")[1]), 
-							session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-FF-SCORECARD":
+					whichInning = Integer.valueOf(valueToProcess.split(",")[1]);
+					
+					this_doad.populateScorecard(print_writer, viz_scene_path, whichInning, 
+							session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-BOWLINGCARD":
-					this_doad.populateBowlingcard(print_writer, valueToProcess.split(",")[0], Integer.valueOf(valueToProcess.split(",")[1]), 
-							session_match, session_selected_broadcaster, viz_scene_path);
+				case "POPULATE-FF-BOWLINGCARD":
+					whichInning = Integer.valueOf(valueToProcess.split(",")[1]);
+					
+					this_doad.populateBowlingcard(print_writer, viz_scene_path, whichInning, 
+							session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-PARTNERSHIP":
-					this_doad.populatePartnership(print_writer, valueToProcess.split(",")[0], Integer.valueOf(valueToProcess.split(",")[1]), 
-							session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-FF-PARTNERSHIP":
+					whichInning = Integer.valueOf(valueToProcess.split(",")[1]);
+					
+					this_doad.populatePartnership(print_writer, viz_scene_path, whichInning, 
+							session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-MATCHSUMMARY":
-					this_doad.populateMatchsummary(print_writer, valueToProcess.split(",")[0], Integer.valueOf(valueToProcess.split(",")[1]), 
-							session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-FF-MATCHSUMMARY":
+					whichInning = Integer.valueOf(valueToProcess.split(",")[1]);
+					
+					this_doad.populateMatchsummary(print_writer, viz_scene_path, whichInning, 
+							session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-BUG":
-					this_doad.populateBug(print_writer, valueToProcess.split(",")[0],Integer.valueOf(valueToProcess.split(",")[1]), 
-							valueToProcess.split(",")[2],Integer.valueOf(valueToProcess.split(",")[3]), session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-L3-BUG":
+					this_doad.populateBug(print_writer, viz_scene_path,Integer.valueOf(valueToProcess.split(",")[1]), 
+							valueToProcess.split(",")[2],Integer.valueOf(valueToProcess.split(",")[3]), session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-HOWOUT":
-					this_doad.populateHowout(print_writer, valueToProcess.split(",")[0],Integer.valueOf(valueToProcess.split(",")[1]), valueToProcess.split(",")[2], 
-							Integer.valueOf(valueToProcess.split(",")[3]), session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-L3-HOWOUT":
+					whichInning = Integer.valueOf(valueToProcess.split(",")[1]);
+					stats_type = valueToProcess.split(",")[2];
+					player_id = Integer.valueOf(valueToProcess.split(",")[3]);
+					
+					this_doad.populateHowout(print_writer, viz_scene_path,whichInning, stats_type, player_id, session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-PLAYERSTATS":
-					this_doad.populatePlayerstats(print_writer, valueToProcess.split(",")[0],Integer.valueOf(valueToProcess.split(",")[1]), valueToProcess.split(",")[2], 
-							Integer.valueOf(valueToProcess.split(",")[3]), session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-L3-PLAYERSTATS":
+					whichInning = Integer.valueOf(valueToProcess.split(",")[1]);
+					stats_type = valueToProcess.split(",")[2];
+					player_id = Integer.valueOf(valueToProcess.split(",")[3]);
+					
+					this_doad.populatePlayerstats(print_writer, viz_scene_path,whichInning, stats_type, 
+							player_id, session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-NAMESUPER":
-					this_doad.populateNameSuper(print_writer, valueToProcess.split(",")[0],Integer.valueOf(valueToProcess.split(",")[1]),valueToProcess.split(",")[2],
-							Integer.valueOf(valueToProcess.split(",")[3]), session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-L3-NAMESUPER":
+					this_doad.populateNameSuper(print_writer, viz_scene_path,Integer.valueOf(valueToProcess.split(",")[1]),valueToProcess.split(",")[2],
+							Integer.valueOf(valueToProcess.split(",")[3]), session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-PLAYERPROFILE":
-					for(Statistics stats : cricketService.getPlayerStatistics(Integer.valueOf(valueToProcess.split(",")[1]))) {
+				case "POPULATE-FF-PLAYERPROFILE":
+					player_id = Integer.valueOf(valueToProcess.split(",")[1]);
+					stats_type = valueToProcess.split(",")[2];
+					type_of_profile = valueToProcess.split(",")[3];
+					
+					for(Statistics stats : cricketService.getPlayerStatistics(player_id)) {
 						stats.setStats_type(cricketService.getStatsType(stats.getStats_type_id()));
-						if(stats.getStats_type().getStats_short_name().equalsIgnoreCase(valueToProcess.split(",")[2])) {
-							this_doad.populatePlayerProfile(print_writer,valueToProcess.split(",")[0],Integer.valueOf(valueToProcess.split(",")[1]),
-									valueToProcess.split(",")[2],valueToProcess.split(",")[3],stats,session_match, session_selected_broadcaster , viz_scene_path);
+						stats = updateStatisticsWithMatchData(stats, session_match, type_of_profile);
+						if(stats.getStats_type().getStats_short_name().equalsIgnoreCase(stats_type)) {
+							this_doad.populatePlayerProfile(print_writer,viz_scene_path,player_id,
+									stats_type,type_of_profile,stats,session_match, session_selected_broadcaster);
 						}
 					}
 					break;
-				case "POPULATE-DOUBLETEAMS":
-					this_doad.populateDoubleteams(print_writer,valueToProcess.split(",")[0], session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-FF-DOUBLETEAMS":
+					this_doad.populateDoubleteams(print_writer,viz_scene_path, session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-INFOBAR":
+				case "POPULATE-L3-INFOBAR":
+					
+					top_left_stats = valueToProcess.split(",")[1];
+					top_right_stats = valueToProcess.split(",")[2];
 					info_bar_bottom_left = valueToProcess.split(",")[3];
 					info_bar_bottom_right = valueToProcess.split(",")[4];
-					this_doad.populateInfobar(print_writer, valueToProcess.split(",")[0],valueToProcess.split(",")[1], valueToProcess.split(",")[2],valueToProcess.split(",")[3],
-							valueToProcess.split(",")[4],last_infobar_batsman, session_match, session_selected_broadcaster,last_bowler);
+					this_doad.populateInfobar(print_writer, viz_scene_path,top_left_stats,top_right_stats,info_bar_bottom_left,
+							info_bar_bottom_right,last_infobar_batsman, session_match, session_selected_broadcaster,last_infobar_bowler);
 					break;
 				case "POPULATE-INFOBAR-BOTTOMLEFT":
 					this_doad.processAnimation(print_writer, "Section4_Out", "CONTINUE", session_selected_broadcaster);
@@ -253,7 +314,6 @@ public class IndexController
 						}
 					}
 					this_doad.processAnimation(print_writer, "Section4_In", "START", session_selected_broadcaster);
-					//this_doad.processAnimation(print_writer, "Section5_In", "START", session_selected_broadcaster);
 					info_bar_bottom_left = valueToProcess;
 					break;
 				case "POPULATE-INFOBAR-BOTTOMRIGHT":
@@ -265,13 +325,10 @@ public class IndexController
 							this_doad.populateInfobarBottomRight(false,print_writer, valueToProcess, session_match, session_selected_broadcaster);
 						}
 					}
-					//this_doad.processAnimation(print_writer, "Section4_In", "START", session_selected_broadcaster);
 					this_doad.processAnimation(print_writer, "Section5_In", "START", session_selected_broadcaster);
 					info_bar_bottom_right = valueToProcess;
 					break;
 				case "POPULATE-INFOBAR-BOTTOM":
-					//this_doad.processAnimation(print_writer, "Section4_Out", "CONTINUE", session_selected_broadcaster);
-					//this_doad.processAnimation(print_writer, "Section5_Out", "CONTINUE", session_selected_broadcaster);
 					this_doad.processAnimation(print_writer, "Section6_Out", "CONTINUE", session_selected_broadcaster);
 					for(Inning inn : session_match.getInning()) {
 						if(inn.getIsCurrentInning().equalsIgnoreCase(CricketUtil.YES)) {
@@ -280,21 +337,21 @@ public class IndexController
 					}
 					this_doad.processAnimation(print_writer, "Section6_In", "START", session_selected_broadcaster);
 					info_bar_bottom = valueToProcess;
+					//System.out.println("info_bar_bottom = "+valueToProcess);
 
 					break;
-				case "POPULATE-MATCHID":
-					this_doad.populateMatchId(print_writer,valueToProcess, session_match, session_selected_broadcaster , viz_scene_path);
+				case "POPULATE-FF-MATCHID":
+					this_doad.populateMatchId(print_writer,viz_scene_path, session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-PLAYINGXI":
-					this_doad.populatePlayingXI(print_writer, valueToProcess.split(",")[0], Integer.valueOf(valueToProcess.split(",")[1]), 
-							session_match, session_selected_broadcaster, viz_scene_path);
+				case "POPULATE-FF-PLAYINGXI":
+					this_doad.populatePlayingXI(print_writer, viz_scene_path, Integer.valueOf(valueToProcess.split(",")[1]), 
+							session_match, session_selected_broadcaster);
 					break;
-				case "POPULATE-LEADERBOARD":
-					this_doad.populateLeaderBoard(print_writer, valueToProcess.split(",")[0], valueToProcess.split(",")[1], valueToProcess.split(",")[2],
-							session_match, session_selected_broadcaster, viz_scene_path);
+				case "POPULATE-FF-LEADERBOARD":
+					this_doad.populateLeaderBoard(print_writer, viz_scene_path, valueToProcess.split(",")[1], valueToProcess.split(",")[2],
+							session_match, session_selected_broadcaster);
 					break;
 				}
-				
 				return JSONObject.fromObject(this_doad).toString();
 			}
 		case "ANIMATE-IN-SCORECARD": case "ANIMATE-IN-BOWLINGCARD": case "ANIMATE-IN-PARTNERSHIP": case "ANIMATE-IN-MATCHSUMARRY": case "ANIMATE-IN-BUG": case "ANIMATE-IN-HOWOUT": 
@@ -427,5 +484,43 @@ public class IndexController
 		default:
 			return JSONObject.fromObject(null).toString();
 		}
+	}
+	public Statistics updateStatisticsWithMatchData(Statistics stat, Match match, String typeOfProfile)
+	{
+		if(stat.getStats_type().getStats_short_name().equalsIgnoreCase(match.getMatchType())) {
+			stat.setMatches(stat.getMatches() + 1);
+			for(Inning inn : match.getInning())
+			{
+				switch(typeOfProfile.toUpperCase()) {
+				case CricketUtil.BATSMAN:
+					for(BattingCard bc : inn.getBattingCard()) {
+						if(bc.getPlayerId() == stat.getPlayer_id()) {
+							if(bc.getBatsmanInningStarted() == CricketUtil.YES) {
+								stat.setInnings(stat.getInnings() + 1);
+							}
+							stat.setRuns(stat.getRuns() + bc.getRuns());
+							if(bc.getRuns() < 100 && bc.getRuns() >= 50) {
+								stat.setFifties(stat.getFifties() + 1);
+							}else if(bc.getRuns() >= 100){
+								stat.setHundreds(stat.getHundreds() + 1);
+							}
+						}
+					}
+					break;
+				case CricketUtil.BOWLER:
+					if(inn.getBowlingCard() != null && inn.getBowlingCard().size()>0) {
+						for(BowlingCard boc : inn.getBowlingCard()) {
+							if(boc.getPlayerId() == stat.getPlayer_id()) {
+								stat.setWickets(stat.getWickets() + boc.getWickets());
+								stat.setBest_figures(stat.getBest_figures());
+							}
+						}							
+					}
+					break;
+				
+				}
+			}
+		}
+		return stat;
 	}
 }
